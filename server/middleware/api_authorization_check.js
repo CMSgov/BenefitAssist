bcc90a330fa544b623c5ce52dd30c5c6a67d369a
Miller, Greg <Greg_Miller@intuit.com>
2016-09-05 17:49:43 -0700

    add middleware to verify API keys (if enabled in the settings)

---
+++ server/middleware/api_authorization_check.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var Settings = require('../config/settings');
+var _ = require('lodash');
+var Logger = require('../logging/logger').getLogger("Middleware");
+
+/*
+ * Middleware to validate requests have a valid api key in the 'Authorization' header
+ *
+ * returns a 403 if invalid
+ */
+module.exports = function (req, resp, next) {
+    if (Settings.server.authorizationEnabled === false) {
+        return next();
+    }
+
+    // get the authorization header
+    var auth = req.headers.authorization;
+    if (_.has(Constants.apiKeys, auth)) {
+        Logger.info("Authorization allowed for user: " + Constants.apiKeys[auth] + ', for route: ' + req.url);
+        next();
+    }
+    else {
+        Logger.info("Authorization failed for token: " + auth);
+        resp.writeHead(403);
+        resp.end();
+    }
+};
+
