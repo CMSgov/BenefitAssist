258084d626163e4803f273c00c572c5752be7ab3
claytongulick <claytongulick@gmail.com>
2018-04-23 13:20:12 -0500

    updated dependencies and modernized some code

--- server/restify_server.js
+++ server/restify_server.js
@@ -7,78 +7,63 @@ var restify = require('restify'),
     requestLogger = require('./middleware/request_logger'),
     noCache = require('./middleware/noCache'),
     testCheck = require('./middleware/test_check'),
-    cors = require('./middleware/cors'),
     Constants = require('./config/constants'),
-    logger = require('./logging/logger').getLogger();
-
+    logger = require('./logging/logger').getLogger(),
+    Settings = require('./config/settings'),
+    corsMiddleware = require('restify-cors-middleware');
 
 // Set http global settings so we don't run out of sockets
 https.globalAgent.maxSockets = Infinity;
 http.globalAgent.maxSockets = Infinity;
 
-
-/**
- * Application server
- */
-function RestifyServer (name) {
-    if (!(this instanceof RestifyServer)) {
-        return new RestifyServer(name);
+class RestifyServer {
+    constructor(name) {
+        this.server = null;
+        this.name = name;
     }
 
-    this.server = null;
-    this.name = name;
-}
-
-/**
- *  Start method
- */
-RestifyServer.prototype = {
-    start : function (port) {
-        var self = this;
+    start(port) {
+        let self = this;
 
         this.server = restify.createServer({ name : this.name});
 
-        // setup CORS to allow versioning
-        restify.CORS.ALLOW_HEADERS.push('accept-version');
-        restify.CORS.ALLOW_HEADERS.push('authorization');
-        restify.CORS.ALLOW_HEADERS.push('x-requested-with');
-        restify.CORS.ALLOW_HEADERS.push('accept');
+        const cors = corsMiddleware({
+            preflightMaxAge: 5, //Optional
+            origins: Settings.server.origins,
+            // setup CORS to allow versioning
+            allowHeaders: ['accept-version', 'authorization', 'x-requested-with','accept'],
+        })
+
         this.server
+            .pre(cors.preflight)
             .use(requestLogger)     // log all requests
-            .use(restify.CORS())    // enable cross domain handling
+            .use(cors.actual)    // enable cross domain handling
             .use(noCache)           // don't cache requests
-            .use(restify.fullResponse())
-            .use(restify.bodyParser({
+            .use(restify.plugins.bodyParser({
                 mapParams : false   // Query string parameters will not be in body parser, but in query string parser
             }))
-            .use(restify.queryParser())
+            .use(restify.plugins.queryParser())
             .use(require('compression')())
             .use(require('cookie-parser')())
-            .use(cors)              // enable CORS for domains specified in our settings
             .use(testCheck);        // check all requests for the test flag
-
-
 
         Router.init(this.server);  // Initialize all our RESTFul APIs
 
         this.server.listen(port, function () {
             logger.info('Starting Restify HTTP server ' + self.name + ' running on port ' + port);
         });
+    }
 
-    },
-
-    /**
-     *  Stop the server
-     */
-    stop : function () {
+    stop() {
         logger.info('Stopping Restify HTTP server ' + this.name);
         this.server.close();
-    },
+    }
 
-    on : function (eventName, callback) {
+    on(eventName, callback) {
         return this.server.on(eventName, callback);
     }
-};
+}
+
 
 module.exports = RestifyServer;
 
